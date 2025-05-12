8cfc271e36d35de04224741641e1708abb78573b
leo <leonard.aronsson@skola.taby.se>
2025-05-12 11:34:30 +0200

    vote reset fix

--- game/game.js
+++ game/game.js
@@ -17,6 +17,7 @@ class Game {
                     this.io.emit('game_over', this.winner);
                     console.log('game over');
                     clearInterval(this.loop);
+                    this.loop = null; // Reset loop after stopping
                 }
                 this.io.emit('ball_update', this.ball);
             }, 1000 / 60);
@@ -27,6 +28,7 @@ class Game {
         this.vote_count = 0;
         this.winner = null;
         this.io.emit('reset', this.ball);
+        this.start_game_loop();
     }
 }
 
